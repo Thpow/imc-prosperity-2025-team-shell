4004f7778a65f58cd5e69affdf43837d88d5652b
twpowel <72366532+Thpow@users.noreply.github.com>
2025-03-07 18:32:48 -0500

    moving average

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from scipy.stats import norm
 
 from funcs import *
 
-product = "RAINFOREST_RESIN"
+product = "KELP"
 
 class Trader:
     def __init__(self):
@@ -20,22 +20,20 @@ class Trader:
         self.position_limit = 50
 
         #=======history tacking=======#
-        self.price_history = {}
-        self.volatility_history = {}
+        self.price_history = {"KELP":[]}
+        self.volatility_history = {"KELP":[]}
         self.position_limit = 50
-        self.price_history = {}
-        self.volatility_history = {}
         self.low = {}
         self.high = {}
 
         #=======params=======#
         self.max_history_length = 200
         self.max_volatility_length = 200   
-        short_ma_window = 50
-        long_ma_window = 200
+        self.short_ma_window = 50
+        self.long_ma_window = 200
         
     def calculate_ma(self, prices, window):
-        pass
+        return sum(prices)/window
 
     def run(self, state: TradingState):
         #init
@@ -47,18 +45,23 @@ class Trader:
 
         #update historic data
         self.price_history[product].append(calculate_mid_price(order_depth))
+        if len(self.price_history) > 200:
+            self.price_history.pop(0)
 
         #indicators
 
         #check if its been 200 days
-        short_MA = self.calculate_ma(self.price_history[-1*self.short_ma_window:], self.short_ma_window)
-        long_MA = self.calculate_ma(self.price_history, self.long_ma_window)
+        if state.timestamp >=200:
+            short_MA = self.calculate_ma(self.price_history[product][-self.short_ma_window:], self.short_ma_window)
+            long_MA = self.calculate_ma(self.price_history[product], self.long_ma_window)
 
         # get best price and how many orders
         best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-        
-                                        #price #quant
-        result[product] = [Order(product, best_bid, -50)]
+        if state.timestamp >=200:
+            if short_MA > long_MA:
+                                            #price #quant
+                result[product] = [Order(product, best_bid, 50)]
+    
         
     
         return result, 0, ""
